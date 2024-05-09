2af8a440f88b797c47f5e6491c15d9453ce957bb
Aaryan Pashine <pashineaaryan@gmail.com>
2024-05-09 12:39:16 -0400

    biggggg images

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let rotation = 0;
 
 let cur_id = 0;
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const draggableElement = document.querySelector("#draggableElement");
   create_draggable(draggableElement, draggableElement);
 
@@ -12,20 +12,24 @@ document.addEventListener("DOMContentLoaded", function() {
   draggableElement.appendChild(image_viewer);
   image_viewer.innerHTML = "";
 
-  draggableElement.onmouseenter = function(e) {
+  draggableElement.onmouseenter = function (e) {
     draggableElement.style.border = "1px solid red";
   };
 
-  draggableElement.onmouseleave = function(e) {
+  draggableElement.onmouseleave = function (e) {
     draggableElement.style.border = "none";
   };
 
+  blue_book_imgs.forEach((img) => {
+    image_viewer.appendChild(img);
+    img.style.display = "none";
+  });
   set_image(0);
 
   add_corners(draggableElement, draggableElement);
 });
 
-document.addEventListener("keydown", function(e) {
+document.addEventListener("keydown", function (e) {
   if (e.key === "ArrowRight") {
     increment_image();
   }
@@ -36,8 +40,12 @@ document.addEventListener("keydown", function(e) {
 });
 
 function set_image(id) {
-  image_viewer.innerHTML = "";
-  image_viewer.appendChild(blue_book_imgs[id]);
+  blue_book_imgs.forEach((img) => {
+    img.style.display = "none";
+  });
+  blue_book_imgs[id].style.display = "block";
+  // image_viewer.innerHTML = "";
+  // image_viewer.appendChild(blue_book_imgs[id]);
 }
 
 function increment_image() {
@@ -79,7 +87,7 @@ function add_corners(elem, parent) {
     corner_div.style.height = "10px";
     corner_div.style.position = "absolute";
     corner_div.style.backgroundColor = "red";
-    corner_div.onclick = function(e) {
+    corner_div.onclick = function (e) {
       e.stopPropagation();
       rotation += 180;
       update_rotation(parent, rotation);
@@ -117,13 +125,13 @@ function create_draggable(dragable_elem, draggable_part) {
   let offsetX = 0;
   let offsetY = 0;
 
-  draggable_part.addEventListener("pointerdown", function(e) {
+  draggable_part.addEventListener("pointerdown", function (e) {
     isDragging = true;
     offsetX = e.clientX - dragable_elem.getBoundingClientRect().left;
     offsetY = e.clientY - dragable_elem.getBoundingClientRect().top;
   });
 
-  draggable_part.addEventListener("pointermove", function(e) {
+  draggable_part.addEventListener("pointermove", function (e) {
     if (!isDragging) return;
     let x = e.clientX - offsetX;
     let y = e.clientY - offsetY;
@@ -132,11 +140,11 @@ function create_draggable(dragable_elem, draggable_part) {
     update_position(dragable_elem, x, y, r);
   });
 
-  draggable_part.addEventListener("pointerup", function() {
+  draggable_part.addEventListener("pointerup", function () {
     isDragging = false;
   });
 
-  draggable_part.addEventListener("onmouseleave", function() {
+  draggable_part.addEventListener("onmouseleave", function () {
     isDragging = false;
   });
 }
